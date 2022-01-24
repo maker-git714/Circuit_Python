fa6ee3566124e39f0d14f3175c22d0a2c512241b
maker-git714 <71176174+maker-git714@users.noreply.github.com>
2022-01-23 22:05:58 -0500

    delete redundant wifi connection code

--- ESP32-S2-IO-outdoor-feeds.py
+++ ESP32-S2-IO-outdoor-feeds.py
@@ -41,91 +41,6 @@ i2c_power = digitalio.DigitalInOut(board.I2C_POWER_INVERTED)
 i2c_power.switch_to_output()
 i2c_power.value = False
 
-''' ### WiFi ###
-
-# Add a secrets.py to your filesystem that has a dictionary called secrets with "ssid" and
-# "password" keys with your WiFi credentials. DO NOT share that file or commit it into Git or other
-# source control.
-try:
-    from secrets import secrets
-except ImportError:
-    print("WiFi secrets are kept in secrets.py, please add them there!")
-    raise
-
-# Set your Adafruit IO Username and Key in secrets.py
-# (visit io.adafruit.com if you need to create an account,
-# or if you need your Adafruit IO key.)
-aio_username = secrets["aio_username"]
-aio_key = secrets["aio_key"]
-
-print("Connecting to %s" % secrets["ssid"])
-wifi.radio.connect(secrets["ssid"], secrets["password"])
-print("Connected to %s!" % secrets["ssid"])
-
-# Define callback functions which will be called when certain events happen.
-# pylint: disable=unused-argument
-def connected(client):
-    # Connected function will be called when the client is connected to Adafruit IO.
-    # This is a good place to subscribe to feed changes.  The client parameter
-    # passed to this function is the Adafruit IO MQTT client so you can make
-    # calls against it easily.
-    print("Connected to Adafruit IO!  Listening for Feed changes...")
-    # Subscribe to changes on feeds.
-    client.subscribe("humidity")
-    client.subscribe("temperature")
-    client.subscribe("pressure")
-
-
-def subscribe(client, userdata, topic, granted_qos):
-    # This method is called when the client subscribes to a new feed.
-    print("Subscribed to {0} with QOS level {1}".format(topic, granted_qos))
-
-
-def unsubscribe(client, userdata, topic, pid):
-    # This method is called when the client unsubscribes from a feed.
-    print("Unsubscribed from {0} with PID {1}".format(topic, pid))
-
-
-# pylint: disable=unused-argument
-def disconnected(client):
-    # Disconnected function will be called when the client disconnects.
-    print("Disconnected from Adafruit IO!")
-
-
-# pylint: disable=unused-argument
-def message(client, feed_id, payload):
-    # Message function will be called when a subscribed feed has a new value.
-    # The feed_id parameter identifies the feed, and the payload parameter has
-    # the new value.
-    print("Feed {0} received new value: {1}".format(feed_id, payload))
-
-
-# Create a socket pool
-pool = socketpool.SocketPool(wifi.radio)
-
-# Initialize a new MQTT Client object
-mqtt_client = MQTT.MQTT(
-    broker="io.adafruit.com",
-    username=secrets["aio_username"],
-    password=secrets["aio_key"],
-    socket_pool=pool,
-    ssl_context=ssl.create_default_context(),
-)
-
-# Initialize an Adafruit IO MQTT Client
-io = IO_MQTT(mqtt_client)
-
-# Connect the callback methods defined above to Adafruit IO
-io.on_connect = connected
-io.on_disconnect = disconnected
-io.on_subscribe = subscribe
-io.on_unsubscribe = unsubscribe
-io.on_message = message
-
-# Connect to Adafruit IO
-print("Connecting to Adafruit IO...")
-io.connect()
-'''
 
 # Create instance of Neopixel
 pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
@@ -230,7 +145,7 @@ try:
     send_io_data(setup_feed("battery-percent"), battery_percent)
     print("Data sent!")
     # Turn off the Neopixel to indicate data sending is complete.
-    neopixel.fill((0,0,0))
+    pixel.fill((0,0,0))
 
 # Adafruit IO can fail with multiple errors depending on the situation, so this except is broad.
 except Exception as e:  # pylint: disable=broad-except
